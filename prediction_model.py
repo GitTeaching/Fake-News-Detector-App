ac75edfb6942d95fbfc43986e0402385bf17168f
A. Aid <aid_aicha@hotmail.fr>
2020-07-22 14:24:34 +0100

    Update prediction_model.py

---
+++ prediction_model.py
@@ -0,0 +1,42 @@
+from nltk.corpus import stopwords
+#from sklearn.externals import joblib
+import joblib
+import nltk
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+nltk.data.path.append('./nltk_data')
+
+
+model = joblib.load('model.pkl')
+print('=> Pickle Loaded : Model ')
+
+
+class PredictionModel:
+    output = {}
+
+    # constructor
+    def __init__(self, original_text):
+        self.output['original'] = original_text
+
+
+    # predict
+    def predict(self):
+        review = self.preprocess()
+        tfidfvect = joblib.load('tfidfvect.pkl')
+        print('=> Pickle Loaded : Vectorizer')
+        text_vect = tfidfvect.transform([review]).toarray()
+        self.output['prediction'] = 'FAKE' if model.predict(text_vect) == 0 else 'REAL'
+        return self.output
+
+
+    # Helper methods
+    def preprocess(self):
+        review = re.sub('[^a-zA-Z]', ' ', self.output['original'])
+        review = review.lower()
+        review = review.split()
+        review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+        review = ' '.join(review)
+        self.output['preprocessed'] = review
+        return review
